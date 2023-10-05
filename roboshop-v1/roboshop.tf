4ded1996ee59e8241f91a6a4badbe624542d963c
LAPTOP-7BJSCJKS\ashok <ashok.csecr@gmail.com>
2023-10-05 20:40:52 +0530

    terraform

--- roboshop-v1/roboshop.tf
+++ roboshop-v1/roboshop.tf
@@ -8,6 +8,15 @@ resource "aws_instance" "frontend" {
   }
 }
 
+resource "aws_route53_record" "frontend" {
+  zone_id = Z02904712FMTLMWVAWWYZ
+  name    = "frontend-dev.gudishivadevops.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.frontend.private_ip]
+}
+
+
 resource "aws_instance" "mongodb" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.small"
