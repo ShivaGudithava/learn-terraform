c3a1763f2dac97ede067d59931cb0d070b040b96
LAPTOP-7BJSCJKS\ashok <ashok.csecr@gmail.com>
2023-10-06 18:13:45 +0530

    terraform

--- 06-loops/count/main.tf
+++ 06-loops/count/main.tf
@@ -2,16 +2,21 @@ variable "components" {
   default = ["frontend", "mongodb", "catalogue"]
 }
 
-resource "aws_instance" "instance" {
+#resource "aws_instance" "instance" {
+#
+#  count = length(var.components)
+#
+#  ami           = "ami-03265a0778a880afb"
+#  instance_type = "t3.small"
+#  vpc_security_group_ids = [ "sg-0634b360d4d7dc216" ]
+#
+#  tags = {
+#    // name = var.component[count.index]
+#    Name = element(var.components, count.index)
+#  }
+#}
 
+resource "aws_security_group" "allow_tls" {
   count = length(var.components)
-
-  ami           = "ami-03265a0778a880afb"
-  instance_type = "t3.small"
-  vpc_security_group_ids = [ "sg-0634b360d4d7dc216" ]
-
-  tags = {
-    // name = var.component[count.index]
-    Name = element(var.components, count.index)
-  }
+  name        = element(var.components,count.index)
 }
