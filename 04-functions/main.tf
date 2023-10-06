30440070686d543d944031ea1d955294bc6ebe4b
LAPTOP-7BJSCJKS\ashok <ashok.csecr@gmail.com>
2023-10-06 16:09:44 +0530

    terraform

--- 04-functions/main.tf
+++ 04-functions/main.tf
@@ -6,3 +6,20 @@ output "fruits" {
   //value =  var.fruits[2]
   value = element(var.fruits,2)
 }
+
+variable "fruits_with-stock" {
+  default = {
+    apple = {
+      stock = 100
+    }
+  }
+}
+
+output "fruit_stock" {
+  value = try(var.fruits_with-stock["banana"],0)
+}
+
+output "fruit_stock_price" {
+  value = lookup(var.fruits_with-stock["apple"],"price", 2)
+     // try(var.fruit-with_stock["apple"].price, 2)
+}
