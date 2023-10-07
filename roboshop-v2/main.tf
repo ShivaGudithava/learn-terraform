aff2c85a311ea83259c2e595a2483d2b30d61c6a
LAPTOP-7BJSCJKS\ashok <ashok.csecr@gmail.com>
2023-10-07 15:21:09 +0530

    terraform

--- roboshop-v2/main.tf
+++ roboshop-v2/main.tf
@@ -41,11 +41,11 @@ resource "aws_instance" "instance" {
   }
 }
 
-#resource "aws_route53_record" "record" {
-#  for_each = var.components
-#  zone_id = var.zone_id
-#  name    = "${lookup(each.value,"name", null)}.gudishivadevops.online"
-#  type    = "A"
-#  ttl     = 30
-#  records = [lookup(lookup(aws_instance.instance, each.key, null ),"private_ip",) ]
-#}
+resource "aws_route53_record" "record" {
+  for_each = var.components
+  zone_id = var.zone_id
+  name    = "${lookup(each.value,"name", null)}.gudishivadevops.online"
+  type    = "A"
+  ttl     = 30
+  records = [lookup(lookup(aws_instance.instance, each.key, null ),"private_ip",) ]
+}
